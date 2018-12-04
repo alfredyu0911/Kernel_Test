fbb29ffeba731d328f1d85446329e434480262ea
alfredyu0911 <37465693+alfredyu0911@users.noreply.github.com>
2018-12-04 12:35:59 +0800

    Create syscalls.h

---
+++ include/linux/syscalls.h
@@ -0,0 +1,5 @@
+asmlinkage long sys_ayutest0(int pid, unsigned long *addr, unsigned long arySize);
+
+asmlinkage long sys_ayutest1(int pid, unsigned long *addr, unsigned long arySize);
+
+asmlinkage long sys_ayutest2(int pid, unsigned long *addr, unsigned long arySize);
